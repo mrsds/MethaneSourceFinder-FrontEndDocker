7e92357f303e07bf419ffad234f0fe27a1130972
Steve Zelinka <szelinka@arb.ca.gov>
2020-12-15 08:00:25 -0800

    Adding http paths for nginx routing to UAT api and static dockers

--- docker/makeNgnixConf.sh
+++ docker/makeNgnixConf.sh
@@ -12,9 +12,9 @@ case $ENVIRONMENT_NAME in
   ;;
 
   UAT)
-    export API_HOST=http://methanesourcefinder-34f87bb24f8b2d4e.elb.us-west-2.amazonaws.com
+    export API_HOST=http://methanesourcefinder-5267015e5b5db11c.elb.us-west-2.amazonaws.com
     export API_PORT=8001
-    export STATIC_HOST=http://methanesourcefinder-34f87bb24f8b2d4e.elb.us-west-2.amazonaws.com
+    export STATIC_HOST=http://methanesourcefinder-5267015e5b5db11c.elb.us-west-2.amazonaws.com
     export STATIC_PORT=8002
   ;;
 
